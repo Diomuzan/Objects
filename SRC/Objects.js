8591e6db77e55e1499764df7ed94f4f036be07fb
rincedal <94070451+Diomuzan247@users.noreply.github.com>
2023-04-06 23:32:23 +0200

    Initial commit

---
+++ SRC/Objects.js
@@ -0,0 +1,92 @@
+let hyundaielantra {
+    title: 'Hyundai Elantra 2.0 GLS HB AUT 2003 Grijs',
+    description: 'Wegens verkrijgen auto van de zaak verkopen we deze auto.',
+    
+    price: 2499.00,
+    kilometer: 146.000km,
+    buildYear: 2003
+
+
+
+}
+
+let volkswagentiguan = {
+    title: 'Volkswagen Tiguan 1.4 TSI eHybrid R-Line 245pk DSG, LED P',
+    description: 'Fabrieksgarantie t/m 24-02-2026 of 80.000km',
+    price: 57.900,00
+    kilometer: 1047
+    buildYear: 2022
+}
+
+let ferrarif8tributo = {
+    title: 'Ferrari F8 Tributo, noselift, racing seats, carbon interieur. +'
+    description: 'Dit betreft een zeer fraai en uniek uitgevoerde Ferrari F8 Tributo 3.9 V8 HELE (720pk) in de kleur Grigio Silverstone Metallic. Deze auto is nieuw in Nederland door de Ferrari dealer en verkeert in..'
+    price: 424.950,00
+    kilometer: 1640
+    buildYear: 2022
+}
+
+let bmw1serie {
+    title: 'BMW 1 Serie 120i Aut8 High Executive M-sport (leer, navi, led)'
+    description: 'BMW 1 Serie 120i Aut8 High Executive M-sport (leer, navi, LED, sfeerverlichting)'
+    price: 44.890,00
+    kilometer: 7851
+    buildYear: 2022
+}
+
+let renualtclio {
+    title: 'Renualt Clio 1.0 TCe Intens, Achteruitrij camera, Cruise C'
+    description: 'Pack Easy Link navigatie (PKJ128), Apple CarPlay/Android Auto, lichtmetalen velgen 16", Metaallak (QPA$MW)'
+    price: 23.985,00
+    kilometer: 10
+    buildYear: 2022
+}
+
+let item1 = {
+    title: 'Gourmande aardappelen'
+    description: '1,5kg, Vrij vastkokend'
+    action: 'per zak 1,99'
+    price: 1,99
+}
+
+let item2 = {
+    title: 'Laurieri scrocchi'
+    description: 'Italiaanse toast. Mix, met zeezout of met rosemarijn. OP=OP'
+    action: '0,99 in plaats van 1,99'
+    price: 0,99
+}
+
+let item3 = {
+    title: 'Bonne Maman confiture'
+    description: 'Perzik of aardbei, per pot OP=OP'
+    action: 'per pot 2,99'
+    price: 2,99
+}
+
+let item4 = {
+    title: 'Batard'
+    description: 'Wit, meergranen, pompadour of volkoren'
+    action: 'Per stuk 1,29 ipv 1,69'
+    price: 1,69
+}
+
+let item5 = {
+    title: 'Almhof chocolademousse of choco met slagroom'
+    description: 'Alle varianten. Set 2x70'
+    action: 'per stuk 0,69 ipv 1,49'
+    price: 0,69
+}
+
+let phone = {
+    title: 'Samsung Galaxy A52 (6GB) zwart'
+    price: 289,00
+    specs: '6,5" scherm, 6GB werkgeheugen, 128GB opslag, en Qualcomm Snapdragon 720G'
+    reviews: 'Samsung Galaxy A52 - Een terechte bestseller'
+    alternatives: 'Motorola Moto G71, OnePlus Nord CE 5G, Poco F3, Samsung Galaxy A32 4G, Samsung Galaxy A32 5G'
+}
+
+let laptop = {
+    title: 'Lenovo Legion 5 Pro 16ACH6H (82JQ00QQMH)'
+    price: 1899,00
+    specs: 16" scherm, Nvidia GeForce RTX 3070 16GB werkgeheugen
+}
